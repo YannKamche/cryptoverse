e1bfef51201cb990587149b6cea8372a6d37fdaf
Yann Kamche <yannkamche@gmail.com>
2024-05-17 20:38:51 +0100

    initial commit

---
+++ src/index.js
@@ -0,0 +1,7 @@
+//Starting point of any React application
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import App from './App';
+
+ReactDOM.render(<App />, document.getElementById('root'))
