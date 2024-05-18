a8126f64956e1c85ab521eeceef9c01c3ff5bd1b
Yann Kamche <yannkamche@gmail.com>
2024-05-18 19:28:57 +0100

    working on the Home page layout

--- src/components/Homepage.jsx
+++ src/components/Homepage.jsx
@@ -1,10 +1,22 @@
 import React from 'react'
+import millify from 'millify'
+import { Typography, Row, Col, Statistic } from 'antd'
+import { Link } from 'react-router-dom'
 
+// Destructure the Title component from the Typography object
+const { Title } = Typography
 const Homepage = () => {
   return (
-    <div>
-      
-    </div>
+      <>
+          <Title level={2} className='heading'>Global Crypto Stats</Title>
+          <Row>
+              <Col span={12}><Statistic title="Total Cryptocurrencies" value="" /></Col>
+              <Col span={12}><Statistic title="Total Exchanges" value="" /></Col>
+              <Col span={12}><Statistic title="Total Market Cap" value="" /></Col>
+              <Col span={12}><Statistic title="Total 24h Volume" value="" /></Col>
+              <Col span={12}><Statistic title="Total Markets" value="" /></Col>
+          </Row>
+    </>
   )
 }
 
